cf747479466aa849af13d92fb79279b0f9584171
giawa <giawa@hotmail.com>
2013-12-15 15:35:12 -0800

    Add the first four tutorials to GitHub.

---
+++ OpenGLTutorial1/OpenGLTutorial1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Tao.FreeGlut;
+using OpenGL;
+
+namespace OpenGLTutorial1
+{
+    class Program
+    {
+        private static int width = 1280, height = 720;
+
+        static void Main(string[] args)
+        {
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
+            Glut.glutInitWindowSize(width, height);
+            Glut.glutCreateWindow("OpenGL Tutorial");
+
+            Glut.glutIdleFunc(OnRenderFrame);
+            Glut.glutDisplayFunc(OnDisplay);
+
+            Glut.glutMainLoop();
+        }
+
+        private static void OnDisplay()
+        {
+
+        }
+
+        private static void OnRenderFrame()
+        {
+            Gl.Viewport(0, 0, width, height);
+            Gl.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            Glut.glutSwapBuffers();
+        }
+    }
+}
---
+++ OpenGLTutorial4/OpenGLTutorial4/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Tao.FreeGlut;
+using OpenGL;
+
+namespace OpenGLTutorial4
+{
+    class Program
+    {
+        private static int width = 1280, height = 720;
+        private static ShaderProgram program;
+        private static VBO<Vector3> triangle, square;
+        private static VBO<Vector3> triangleColor, squareColor;
+        private static VBO<int> triangleElements, squareElements;
+        private static System.Diagnostics.Stopwatch watch;
+        private static float angle;
+
+        static void Main(string[] args)
+        {
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
+            Glut.glutInitWindowSize(width, height);
+            Glut.glutCreateWindow("OpenGL Tutorial");
+
+            Glut.glutIdleFunc(OnRenderFrame);
+            Glut.glutDisplayFunc(OnDisplay);
+
+            program = new ShaderProgram(VertexShader, FragmentShader);
+
+            program.Use();
+            program["projection_matrix"].SetValue(Matrix4.CreatePerspectiveFieldOfView(0.45f, (float)width / height, 0.1f, 1000f));
+            program["view_matrix"].SetValue(Matrix4.LookAt(new Vector3(0, 0, 10), Vector3.Zero, Vector3.Up));
+
+            triangle = new VBO<Vector3>(new Vector3[] { new Vector3(0, 1, 0), new Vector3(-1, -1, 0), new Vector3(1, -1, 0) });
+            square = new VBO<Vector3>(new Vector3[] { new Vector3(-1, 1, 0), new Vector3(1, 1, 0), new Vector3(1, -1, 0), new Vector3(-1, -1, 0) });
+
+            triangleElements = new VBO<int>(new int[] { 0, 1, 2 }, BufferTarget.ElementArrayBuffer);
+            squareElements = new VBO<int>(new int[] { 0, 1, 2, 3 }, BufferTarget.ElementArrayBuffer);
+
+            triangleColor = new VBO<Vector3>(new Vector3[] { new Vector3(1, 0, 0), new Vector3(0, 1, 0), new Vector3(0, 0, 1) });
+            squareColor = new VBO<Vector3>(new Vector3[] { new Vector3(0.5, 0.5, 1), new Vector3(0.5, 0.5, 1), new Vector3(0.5, 0.5, 1), new Vector3(0.5, 0.5, 1) });
+
+            watch = System.Diagnostics.Stopwatch.StartNew();
+
+            Glut.glutMainLoop();
+        }
+
+        private static void OnDisplay()
+        {
+
+        }
+
+        private static void OnRenderFrame()
+        {
+            watch.Stop();
+            float deltaTime = (float)watch.ElapsedTicks / System.Diagnostics.Stopwatch.Frequency;
+            watch.Restart();
+
+            angle += deltaTime;
+
+            Gl.Viewport(0, 0, width, height);
+            Gl.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            program.Use();
+
+            // draw my triangle
+            program["model_matrix"].SetValue(Matrix4.CreateRotationY(angle) * Matrix4.CreateTranslation(new Vector3(-1.5f, 0, 0)));
+            Gl.BindBufferToShaderAttribute(triangle, program, "vertexPosition");
+            Gl.BindBufferToShaderAttribute(triangleColor, program, "vertexColor");
+            Gl.BindBuffer(triangleElements);
+
+            Gl.DrawElements(BeginMode.Triangles, triangleElements.Count, DrawElementsType.UnsignedInt, IntPtr.Zero);
+
+            // draw my square
+            program["model_matrix"].SetValue(Matrix4.CreateRotationX(angle) * Matrix4.CreateTranslation(new Vector3(1.5f, 0, 0)));
+            Gl.BindBufferToShaderAttribute(square, program, "vertexPosition");
+            Gl.BindBufferToShaderAttribute(squareColor, program, "vertexColor");
+            Gl.BindBuffer(squareElements);
+
+            Gl.DrawElements(BeginMode.Quads, squareElements.Count, DrawElementsType.UnsignedInt, IntPtr.Zero);
+
+            Glut.glutSwapBuffers();
+        }
+
+        public static string VertexShader = @"
+in vec3 vertexPosition;
+in vec3 vertexColor;
+
+out vec3 color;
+
+uniform mat4 projection_matrix;
+uniform mat4 view_matrix;
+uniform mat4 model_matrix;
+
+void main(void)
+{
+    color = vertexColor;
+    gl_Position = projection_matrix * view_matrix * model_matrix * vec4(vertexPosition, 1);
+}
+";
+
+        public static string FragmentShader = @"
+in vec3 color;
+
+void main(void)
+{
+    gl_FragColor = vec4(color, 1);
+}
+";
+    }
+}
