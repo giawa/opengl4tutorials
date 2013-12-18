c1d3654db301ee452770dad1f904c50776f1f799
Montana Reid <giawa@hotmail.com>
2013-12-18 11:50:22 -0800

    Add Tutorials 5-7, which include 3d shapes, texturing, lighting and some basic keyboard control.

---
+++ OpenGLTutorial5/OpenGLTutorial5/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using Tao.FreeGlut;
+using OpenGL;
+
+namespace OpenGLTutorial5
+{
+    class Program
+    {
+        private static int width = 1280, height = 720;
+        private static ShaderProgram program;
+        private static VBO<Vector3> pyramid, cube;
+        private static VBO<Vector3> pyramidColor, cubeColor;
+        private static VBO<int> pyramidTriangles, cubeQuads;
+        private static System.Diagnostics.Stopwatch watch;
+        private static float angle;
+
+        static void Main(string[] args)
+        {
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
+            Glut.glutInitWindowSize(width, height);
+            Glut.glutCreateWindow("OpenGL Tutorial");
+
+            Glut.glutIdleFunc(OnRenderFrame);
+            Glut.glutDisplayFunc(OnDisplay);
+
+            Glut.glutCloseFunc(OnClose);
+
+            Gl.Enable(EnableCap.DepthTest);
+
+            program = new ShaderProgram(VertexShader, FragmentShader);
+
+            program.Use();
+            program["projection_matrix"].SetValue(Matrix4.CreatePerspectiveFieldOfView(0.45f, (float)width / height, 0.1f, 1000f));
+            program["view_matrix"].SetValue(Matrix4.LookAt(new Vector3(0, 0, 10), Vector3.Zero, Vector3.Up));
+
+            pyramid = new VBO<Vector3>(new Vector3[] {
+                new Vector3(0, 1, 0), new Vector3(-1, -1, 1), new Vector3(1, -1, 1),        // front face
+                new Vector3(0, 1, 0), new Vector3(1, -1, 1), new Vector3(1, -1, -1),        // right face
+                new Vector3(0, 1, 0), new Vector3(1, -1, -1), new Vector3(-1, -1, -1),      // back face
+                new Vector3(0, 1, 0), new Vector3(-1, -1, -1), new Vector3(-1, -1, 1) });   // left face
+            pyramidColor = new VBO<Vector3>(new Vector3[] {
+                new Vector3(1, 0, 0), new Vector3(0, 1, 0), new Vector3(0, 0, 1),
+                new Vector3(1, 0, 0), new Vector3(0, 0, 1), new Vector3(0, 1, 0),
+                new Vector3(1, 0, 0), new Vector3(0, 1, 0), new Vector3(0, 0, 1),
+                new Vector3(1, 0, 0), new Vector3(0, 0, 1), new Vector3(0, 1, 0) });
+            pyramidTriangles = new VBO<int>(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 }, BufferTarget.ElementArrayBuffer);
+
+            cube = new VBO<Vector3>(new Vector3[] {
+                new Vector3(1, 1, -1), new Vector3(-1, 1, -1), new Vector3(-1, 1, 1), new Vector3(1, 1, 1),
+                new Vector3(1, -1, 1), new Vector3(-1, -1, 1), new Vector3(-1, -1, -1), new Vector3(1, -1, -1),
+                new Vector3(1, 1, 1), new Vector3(-1, 1, 1), new Vector3(-1, -1, 1), new Vector3(1, -1, 1),
+                new Vector3(1, -1, -1), new Vector3(-1, -1, -1), new Vector3(-1, 1, -1), new Vector3(1, 1, -1),
+                new Vector3(-1, 1, 1), new Vector3(-1, 1, -1), new Vector3(-1, -1, -1), new Vector3(-1, -1, 1),
+                new Vector3(1, 1, -1), new Vector3(1, 1, 1), new Vector3(1, -1, 1), new Vector3(1, -1, -1) });
+            cubeColor = new VBO<Vector3>(new Vector3[] {
+                new Vector3(0, 1, 0), new Vector3(0, 1, 0), new Vector3(0, 1, 0), new Vector3(0, 1, 0), 
+                new Vector3(1, 0.5, 0), new Vector3(1, 0.5, 0), new Vector3(1, 0.5, 0), new Vector3(1, 0.5, 0),
+                new Vector3(1, 0, 0), new Vector3(1, 0, 0), new Vector3(1, 0, 0), new Vector3(1, 0, 0), 
+                new Vector3(1, 1, 0), new Vector3(1, 1, 0), new Vector3(1, 1, 0), new Vector3(1, 1, 0), 
+                new Vector3(0, 0, 1), new Vector3(0, 0, 1), new Vector3(0, 0, 1), new Vector3(0, 0, 1), 
+                new Vector3(1, 0, 1), new Vector3(1, 0, 1), new Vector3(1, 0, 1), new Vector3(1, 0, 1) });
+            cubeQuads = new VBO<int>(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23 }, BufferTarget.ElementArrayBuffer);
+
+
+            watch = System.Diagnostics.Stopwatch.StartNew();
+
+            Glut.glutMainLoop();
+        }
+
+        private static void OnClose()
+        {
+            pyramid.Dispose();
+            pyramidColor.Dispose();
+            pyramidTriangles.Dispose();
+            cube.Dispose();
+            cubeColor.Dispose();
+            cubeQuads.Dispose();
+            program.DisposeChildren = true;
+            program.Dispose();
+        }
+
+        private static void OnDisplay()
+        {
+
+        }
+
+        private static void OnRenderFrame()
+        {
+            watch.Stop();
+            float deltaTime = (float)watch.ElapsedTicks / System.Diagnostics.Stopwatch.Frequency;
+            watch.Restart();
+
+            angle += deltaTime;
+
+            Gl.Viewport(0, 0, width, height);
+            Gl.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            program.Use();
+
+            // draw my triangle
+            program["model_matrix"].SetValue(Matrix4.CreateRotationY(angle) * Matrix4.CreateTranslation(new Vector3(-1.5f, 0, 0)));
+            Gl.BindBufferToShaderAttribute(pyramid, program, "vertexPosition");
+            Gl.BindBufferToShaderAttribute(pyramidColor, program, "vertexColor");
+            Gl.BindBuffer(pyramidTriangles);
+
+            Gl.DrawElements(BeginMode.Triangles, pyramidTriangles.Count, DrawElementsType.UnsignedInt, IntPtr.Zero);
+
+            // draw my square
+            program["model_matrix"].SetValue(Matrix4.CreateRotationY(angle / 2) * Matrix4.CreateRotationX(angle) * Matrix4.CreateTranslation(new Vector3(1.5f, 0, 0)));
+            Gl.BindBufferToShaderAttribute(cube, program, "vertexPosition");
+            Gl.BindBufferToShaderAttribute(cubeColor, program, "vertexColor");
+            Gl.BindBuffer(cubeQuads);
+            
+            Gl.DrawElements(BeginMode.Quads, cubeQuads.Count, DrawElementsType.UnsignedInt, IntPtr.Zero);
+
+            Glut.glutSwapBuffers();
+        }
+
+        public static string VertexShader = @"
+#version 130
+
+in vec3 vertexPosition;
+in vec3 vertexColor;
+
+out vec3 color;
+
+uniform mat4 projection_matrix;
+uniform mat4 view_matrix;
+uniform mat4 model_matrix;
+
+void main(void)
+{
+    color = vertexColor;
+    gl_Position = projection_matrix * view_matrix * model_matrix * vec4(vertexPosition, 1);
+}
+";
+
+        public static string FragmentShader = @"
+#version 130
+
+in vec3 color;
+
+out vec4 fragment;
+
+void main(void)
+{
+    fragment = vec4(color, 1);
+}
+";
+    }
+}
---
+++ OpenGLTutorial6/OpenGLTutorial6/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Tao.FreeGlut;
+using OpenGL;
+
+namespace OpenGLTutorial6
+{
+    class Program
+    {
+        private static int width = 1280, height = 720;
+        private static ShaderProgram program;
+        private static VBO<Vector3> cube;
+        private static VBO<Vector2> cubeUV;
+        private static VBO<int> cubeQuads;
+        private static Texture crateTexture;
+        private static System.Diagnostics.Stopwatch watch;
+        private static float angle;
+
+        static void Main(string[] args)
+        {
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
+            Glut.glutInitWindowSize(width, height);
+            Glut.glutCreateWindow("OpenGL Tutorial");
+
+            Glut.glutIdleFunc(OnRenderFrame);
+            Glut.glutDisplayFunc(OnDisplay);
+
+            Glut.glutCloseFunc(OnClose);
+
+            Gl.Enable(EnableCap.DepthTest);
+
+            program = new ShaderProgram(VertexShader, FragmentShader);
+
+            program.Use();
+            program["projection_matrix"].SetValue(Matrix4.CreatePerspectiveFieldOfView(0.45f, (float)width / height, 0.1f, 1000f));
+            program["view_matrix"].SetValue(Matrix4.LookAt(new Vector3(0, 0, 10), Vector3.Zero, Vector3.Up));
+
+            crateTexture = new Texture("crate.jpg");
+
+            cube = new VBO<Vector3>(new Vector3[] {
+                new Vector3(1, 1, -1), new Vector3(-1, 1, -1), new Vector3(-1, 1, 1), new Vector3(1, 1, 1),
+                new Vector3(1, -1, 1), new Vector3(-1, -1, 1), new Vector3(-1, -1, -1), new Vector3(1, -1, -1),
+                new Vector3(1, 1, 1), new Vector3(-1, 1, 1), new Vector3(-1, -1, 1), new Vector3(1, -1, 1),
+                new Vector3(1, -1, -1), new Vector3(-1, -1, -1), new Vector3(-1, 1, -1), new Vector3(1, 1, -1),
+                new Vector3(-1, 1, 1), new Vector3(-1, 1, -1), new Vector3(-1, -1, -1), new Vector3(-1, -1, 1),
+                new Vector3(1, 1, -1), new Vector3(1, 1, 1), new Vector3(1, -1, 1), new Vector3(1, -1, -1) });
+            cubeUV = new VBO<Vector2>(new Vector2[] {
+                new Vector2(0, 0), new Vector2(1, 0), new Vector2(1, 1), new Vector2(0, 1),
+                new Vector2(0, 0), new Vector2(1, 0), new Vector2(1, 1), new Vector2(0, 1),
+                new Vector2(0, 0), new Vector2(1, 0), new Vector2(1, 1), new Vector2(0, 1),
+                new Vector2(0, 0), new Vector2(1, 0), new Vector2(1, 1), new Vector2(0, 1),
+                new Vector2(0, 0), new Vector2(1, 0), new Vector2(1, 1), new Vector2(0, 1),
+                new Vector2(0, 0), new Vector2(1, 0), new Vector2(1, 1), new Vector2(0, 1) });
+
+            cubeQuads = new VBO<int>(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23 }, BufferTarget.ElementArrayBuffer);
+
+
+            watch = System.Diagnostics.Stopwatch.StartNew();
+
+            Glut.glutMainLoop();
+        }
+
+        private static void OnClose()
+        {
+            cube.Dispose();
+            cubeUV.Dispose();
+            cubeQuads.Dispose();
+            crateTexture.Dispose();
+            program.DisposeChildren = true;
+            program.Dispose();
+        }
+
+        private static void OnDisplay()
+        {
+
+        }
+
+        private static void OnRenderFrame()
+        {
+            watch.Stop();
+            float deltaTime = (float)watch.ElapsedTicks / System.Diagnostics.Stopwatch.Frequency;
+            watch.Restart();
+
+            angle += deltaTime;
+
+            Gl.Viewport(0, 0, width, height);
+            Gl.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            Gl.UseProgram(program);
+            Gl.BindTexture(crateTexture);
+
+            // draw my square
+            program["model_matrix"].SetValue(Matrix4.CreateRotationY(angle / 2) * Matrix4.CreateRotationX(angle));
+            Gl.BindBufferToShaderAttribute(cube, program, "vertexPosition");
+            Gl.BindBufferToShaderAttribute(cubeUV, program, "vertexUV");
+            Gl.BindBuffer(cubeQuads);
+            
+            Gl.DrawElements(BeginMode.Quads, cubeQuads.Count, DrawElementsType.UnsignedInt, IntPtr.Zero);
+
+            Glut.glutSwapBuffers();
+        }
+
+        public static string VertexShader = @"
+#version 130
+
+in vec3 vertexPosition;
+in vec2 vertexUV;
+
+out vec2 uv;
+
+uniform mat4 projection_matrix;
+uniform mat4 view_matrix;
+uniform mat4 model_matrix;
+
+void main(void)
+{
+    uv = vertexUV;
+    gl_Position = projection_matrix * view_matrix * model_matrix * vec4(vertexPosition, 1);
+}
+";
+
+        public static string FragmentShader = @"
+#version 130
+
+uniform sampler2D texture;
+
+in vec2 uv;
+
+out vec4 fragment;
+
+void main(void)
+{
+    fragment = texture2D(texture, uv);
+}
+";
+    }
+}
