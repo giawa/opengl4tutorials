5fed600a841c1bf8cde499ed290c47bfdf65d47c
giawa <giawa@hotmail.com>
2014-01-04 15:09:18 -0800

    Add tutorial #13 for normal mapping!  Need to clean this up still, but this is an initial commit from the coffee shop.

---
+++ OpenGLTutorial13/OpenGLTutorial13/Program.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Collections.Generic;
+using Tao.FreeGlut;
+using OpenGL;
+
+namespace OpenGLTutorial12
+{
+    class Program
+    {
+        private static int width = 1280, height = 720;
+        private static System.Diagnostics.Stopwatch watch;
+
+        private static ShaderProgram program;
+        private static VBO<Vector3> cube, cubeNormals, cubeTangents;
+        private static VBO<Vector2> cubeUV;
+        private static VBO<int> cubeTriangles;
+        private static Texture brickDiffuse, brickNormals;
+        private static float xangle, yangle;
+        private static bool autoRotate, lighting = true, fullscreen = false, normalMapping = false;
+        private static bool left, right, up, down;
+
+        private static BMFont font;
+        private static ShaderProgram fontProgram;
+        private static FontVAO information;
+
+        static void Main(string[] args)
+        {
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH | Glut.GLUT_MULTISAMPLE);   // multisampling makes things beautiful!
+            Glut.glutInitWindowSize(width, height);
+            Glut.glutCreateWindow("OpenGL Tutorial");
+
+            Glut.glutIdleFunc(OnRenderFrame);
+            Glut.glutDisplayFunc(OnDisplay);
+
+            Glut.glutKeyboardFunc(OnKeyboardDown);
+            Glut.glutKeyboardUpFunc(OnKeyboardUp);
+
+            Glut.glutCloseFunc(OnClose);
+            Glut.glutReshapeFunc(OnReshape);
+
+            Gl.Enable(EnableCap.DepthTest);
+
+            // create our shader program
+            program = new ShaderProgram(VertexShader, FragmentShader);
+
+            // set up the projection and view matrix
+            program.Use();
+            program["projection_matrix"].SetValue(Matrix4.CreatePerspectiveFieldOfView(0.45f, (float)width / height, 0.1f, 1000f));
+            program["view_matrix"].SetValue(Matrix4.LookAt(new Vector3(0, 0, 10), Vector3.Zero, Vector3.Up));
+
+            program["light_direction"].SetValue(new Vector3(0, 0, 1));
+            program["enable_lighting"].SetValue(lighting);
+            program["normalTexture"].SetValue(1);
+            program["enable_mapping"].SetValue(normalMapping);
+
+            brickDiffuse = new Texture("AlternatingBrick-ColorMap.png");
+            brickNormals = new Texture("AlternatingBrick-NormalMap.png");
+
+            Vector3[] vertices = new Vector3[] {
+                new Vector3(1, 1, -1), new Vector3(-1, 1, -1), new Vector3(-1, 1, 1), new Vector3(1, 1, 1),         // top
+                new Vector3(1, -1, 1), new Vector3(-1, -1, 1), new Vector3(-1, -1, -1), new Vector3(1, -1, -1),     // bottom
+                new Vector3(1, 1, 1), new Vector3(-1, 1, 1), new Vector3(-1, -1, 1), new Vector3(1, -1, 1),         // front face
+                new Vector3(1, -1, -1), new Vector3(-1, -1, -1), new Vector3(-1, 1, -1), new Vector3(1, 1, -1),     // back face
+                new Vector3(-1, 1, 1), new Vector3(-1, 1, -1), new Vector3(-1, -1, -1), new Vector3(-1, -1, 1),     // left
+                new Vector3(1, 1, -1), new Vector3(1, 1, 1), new Vector3(1, -1, 1), new Vector3(1, -1, -1) };
+            cube = new VBO<Vector3>(vertices);
+
+            Vector2[] uvs = new Vector2[] {
+                new Vector2(0, 0), new Vector2(1, 0), new Vector2(1, 1), new Vector2(0, 1),
+                new Vector2(0, 0), new Vector2(1, 0), new Vector2(1, 1), new Vector2(0, 1),
+                new Vector2(0, 0), new Vector2(1, 0), new Vector2(1, 1), new Vector2(0, 1),
+                new Vector2(0, 0), new Vector2(1, 0), new Vector2(1, 1), new Vector2(0, 1),
+                new Vector2(0, 0), new Vector2(1, 0), new Vector2(1, 1), new Vector2(0, 1),
+                new Vector2(0, 0), new Vector2(1, 0), new Vector2(1, 1), new Vector2(0, 1) };
+            cubeUV = new VBO<Vector2>(uvs);
+
+            List<int> triangles = new List<int>();
+            for (int i = 0; i < 6; i++)
+            {
+                triangles.Add(i * 4);
+                triangles.Add(i * 4 + 1);
+                triangles.Add(i * 4 + 2);
+                triangles.Add(i * 4);
+                triangles.Add(i * 4 + 2);
+                triangles.Add(i * 4 + 3);
+            }
+            cubeTriangles = new VBO<int>(triangles.ToArray(), BufferTarget.ElementArrayBuffer);
+
+            Vector3[] normals = Geometry.CalculateNormals(vertices, triangles.ToArray());
+            cubeNormals = new VBO<Vector3>(normals);
+
+            Vector3[] tangents = CalculateTangents(vertices, normals, triangles.ToArray(), uvs);
+            cubeTangents = new VBO<Vector3>(tangents);
+
+            //cubeTriangles = new VBO<int>(new int[] { 0, 1, 2, 0, 3, 2, 4, 5, 6, 4, 7, 6,  }, BufferTarget.ElementArrayBuffer);
+            //cubeTriangles = new VBO<int>(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23 }, BufferTarget.ElementArrayBuffer);
+
+            // load the bitmap font for this tutorial
+            font = new BMFont("font24.fnt", "font24.png");
+            fontProgram = new ShaderProgram(BMFont.FontVertexSource, BMFont.FontFragmentSource);
+
+            fontProgram.Use();
+            fontProgram["ortho_matrix"].SetValue(Matrix4.CreateOrthographic(width, height, 0, 1000));
+            fontProgram["color"].SetValue(new Vector3(1, 1, 1));
+
+            information = font.CreateString(fontProgram, "OpenGL  C#  Tutorial  13");
+
+            watch = System.Diagnostics.Stopwatch.StartNew();
+
+            Glut.glutMainLoop();
+        }
+
+        /// <summary>
+        /// Calculate the Tangent array based on the Vertex, Face, Normal and UV data.
+        /// </summary>
+        private static Vector3[] CalculateTangents(Vector3[] vertices, Vector3[] normals, int[] triangles, Vector2[] uvs)
+        {
+            Vector3[] tangents = new Vector3[vertices.Length];
+            Vector3[] tangentData = new Vector3[vertices.Length];
+
+            for (int i = 0; i < triangles.Length / 3; i++)
+            {
+                Vector3 v1 = vertices[triangles[i * 3]];
+                Vector3 v2 = vertices[triangles[i * 3 + 1]];
+                Vector3 v3 = vertices[triangles[i * 3 + 2]];
+
+                Vector2 w1 = uvs[triangles[i * 3]];
+                Vector2 w2 = uvs[triangles[i * 3] + 1];
+                Vector2 w3 = uvs[triangles[i * 3] + 2];
+
+                float x1 = v2.x - v1.x;
+                float x2 = v3.x - v1.x;
+                float y1 = v2.y - v1.y;
+                float y2 = v3.y - v1.y;
+                float z1 = v2.z - v1.z;
+                float z2 = v3.z - v1.z;
+
+                float s1 = w2.x - w1.x;
+                float s2 = w3.x - w1.x;
+                float t1 = w2.y - w1.y;
+                float t2 = w3.y - w1.y;
+                float r = 1.0f / (s1 * t2 - s2 * t1);
+                Vector3 sdir = new Vector3((t2 * x1 - t1 * x2) * r, (t2 * y1 - t1 * y2) * r, (t2 * z1 - t1 * z2) * r);
+
+                tangents[triangles[i * 3]] += sdir;
+                tangents[triangles[i * 3 + 1]] += sdir;
+                tangents[triangles[i * 3 + 2]] += sdir;
+            }
+
+            for (int i = 0; i < vertices.Length; i++)
+                tangentData[i] = (tangents[i] - normals[i] * Vector3.Dot(normals[i], tangents[i])).Normalize();
+
+            return tangentData;
+        }
+
+        private static void OnClose()
+        {
+            cube.Dispose();
+            cubeNormals.Dispose();
+            cubeUV.Dispose();
+            cubeTriangles.Dispose();
+            brickDiffuse.Dispose();
+            brickNormals.Dispose();
+            program.DisposeChildren = true;
+            program.Dispose();
+            fontProgram.DisposeChildren = true;
+            fontProgram.Dispose();
+            font.FontTexture.Dispose();
+            information.Dispose();
+        }
+
+        private static void OnDisplay()
+        {
+
+        }
+
+        private static void OnRenderFrame()
+        {
+            watch.Stop();
+            float deltaTime = (float)watch.ElapsedTicks / System.Diagnostics.Stopwatch.Frequency;
+            watch.Restart();
+
+            // set up the viewport and clear the previous depth and color buffers
+            Gl.Viewport(0, 0, width, height);
+            Gl.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            // perform rotation of the cube depending on the keyboard state
+            if (autoRotate)
+            {
+                xangle += deltaTime / 2;
+                yangle += deltaTime;
+            }
+            if (right) yangle += deltaTime;
+            if (left) yangle -= deltaTime;
+            if (up) xangle -= deltaTime;
+            if (down) xangle += deltaTime;
+
+            // make sure the shader program and texture are being used
+            Gl.UseProgram(program);
+            Gl.ActiveTexture(TextureUnit.Texture1);
+            Gl.BindTexture(brickNormals);
+            Gl.ActiveTexture(TextureUnit.Texture0);
+            Gl.BindTexture(brickDiffuse);
+
+            // set up the model matrix and draw the cube
+            program["model_matrix"].SetValue(Matrix4.CreateRotationY(yangle) * Matrix4.CreateRotationX(xangle));
+            program["enable_lighting"].SetValue(lighting);
+            program["enable_mapping"].SetValue(normalMapping);
+
+            Gl.BindBufferToShaderAttribute(cube, program, "vertexPosition");
+            Gl.BindBufferToShaderAttribute(cubeNormals, program, "vertexNormal");
+            Gl.BindBufferToShaderAttribute(cubeTangents, program, "vertexTangent");
+            Gl.BindBufferToShaderAttribute(cubeUV, program, "vertexUV");
+            Gl.BindBuffer(cubeTriangles);
+
+            Gl.DrawElements(BeginMode.Triangles, cubeTriangles.Count, DrawElementsType.UnsignedInt, IntPtr.Zero);
+
+            // bind the font program as well as the font texture
+            Gl.UseProgram(fontProgram.ProgramID);
+            Gl.BindTexture(font.FontTexture);
+
+            // draw the tutorial information, which is static
+            information.Draw();
+
+            Glut.glutSwapBuffers();
+        }
+
+        private static void OnReshape(int width, int height)
+        {
+            Program.width = width;
+            Program.height = height;
+
+            Gl.UseProgram(program.ProgramID);
+            program["projection_matrix"].SetValue(Matrix4.CreatePerspectiveFieldOfView(0.45f, (float)width / height, 0.1f, 1000f));
+
+            Gl.UseProgram(fontProgram.ProgramID);
+            fontProgram["ortho_matrix"].SetValue(Matrix4.CreateOrthographic(width, height, 0, 1000));
+
+            information.Position = new Vector2(-width / 2 + 10, height / 2 - font.Height - 10);
+        }
+
+        private static void OnKeyboardDown(byte key, int x, int y)
+        {
+            if (key == 'w') up = true;
+            else if (key == 's') down = true;
+            else if (key == 'd') right = true;
+            else if (key == 'a') left = true;
+            else if (key == 27) Glut.glutLeaveMainLoop();
+        }
+
+        private static void OnKeyboardUp(byte key, int x, int y)
+        {
+            if (key == 'w') up = false;
+            else if (key == 's') down = false;
+            else if (key == 'd') right = false;
+            else if (key == 'a') left = false;
+            else if (key == ' ') autoRotate = !autoRotate;
+            else if (key == 'l') lighting = !lighting;
+            else if (key == 'm') normalMapping = !normalMapping;
+            else if (key == 'f')
+            {
+                fullscreen = !fullscreen;
+                if (fullscreen) Glut.glutFullScreen();
+                else
+                {
+                    Glut.glutPositionWindow(0, 0);
+                    Glut.glutReshapeWindow(1280, 720);
+                }
+            }
+        }
+
+
+        public static string VertexShader = @"
+#version 130
+
+in vec3 vertexPosition;
+in vec3 vertexNormal;
+in vec3 vertexTangent;
+in vec2 vertexUV;
+
+uniform vec3 light_direction;
+
+out vec3 normal;
+out vec2 uv;
+out vec3 light;
+
+uniform mat4 projection_matrix;
+uniform mat4 view_matrix;
+uniform mat4 model_matrix;
+uniform bool enable_mapping;
+
+void main(void)
+{
+    normal = normalize((model_matrix * vec4(floor(vertexNormal), 0)).xyz);
+    uv = vertexUV;
+
+    mat3 tbnMatrix = mat3(vertexTangent, cross(vertexTangent, normal), normal);
+    light = (enable_mapping ? light_direction * tbnMatrix : light_direction);
+
+    gl_Position = projection_matrix * view_matrix * model_matrix * vec4(vertexPosition, 1);
+}
+";
+
+        public static string FragmentShader = @"
+#version 130
+
+uniform sampler2D colorTexture;
+uniform sampler2D normalTexture;
+
+uniform bool enable_lighting;
+uniform mat4 model_matrix;
+uniform bool enable_mapping;
+
+in vec3 normal;
+in vec2 uv;
+in vec3 light;
+
+out vec4 fragment;
+
+void main(void)
+{
+    vec3 fragmentNormal = texture2D(normalTexture, uv).xyz * 2 - 1;
+    vec3 selectedNormal = (enable_mapping ? fragmentNormal : normal);
+    float diffuse = max(dot(selectedNormal, light), 0);
+    float ambient = 0.3;
+    float lighting = (enable_lighting ? max(diffuse, ambient) : 1);
+
+    fragment = vec4(lighting * texture2D(colorTexture, uv).xyz, 1);
+}
+";
+    }
+}
