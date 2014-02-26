b3b75b669b13c91b1a8904487543c56b947cab03
Montana Reid <giawa@hotmail.com>
2014-02-25 16:54:30 -0800

    Add tutorial 16.  This is a bit much for one tutorial, so I may break into two or three later.

---
+++ OpenGLTutorial16/OpenGLTutorial16/Program.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using Tao.FreeGlut;
+using OpenGL;
+
+namespace OpenGLTutorial16
+{
+    class Program
+    {
+        private static int width = 1280, height = 720;
+        private static System.Diagnostics.Stopwatch watch;
+
+        private static ShaderProgram program;
+        private static ObjLoader objectFile;
+        private static bool fullscreen = false, wireframe = false;
+        private static bool left, right, up, down, space;
+
+        private static Camera camera;
+
+        private static BMFont font;
+        private static ShaderProgram fontProgram;
+        private static FontVAO information;
+
+        static void Main(string[] args)
+        {
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH | Glut.GLUT_ALPHA | Glut.GLUT_STENCIL | Glut.GLUT_MULTISAMPLE);
+            Glut.glutInitWindowSize(width, height);
+            Glut.glutCreateWindow("OpenGL Tutorial");
+
+            Gl.Enable(EnableCap.DepthTest);
+            //Gl.Enable(EnableCap.CullFace);
+            Gl.Enable(EnableCap.Multisample);
+
+            Glut.glutIdleFunc(OnRenderFrame);
+            Glut.glutDisplayFunc(OnDisplay);
+
+            Glut.glutKeyboardFunc(OnKeyboardDown);
+            Glut.glutKeyboardUpFunc(OnKeyboardUp);
+
+            Glut.glutCloseFunc(OnClose);
+            Glut.glutReshapeFunc(OnReshape);
+
+            // add our mouse callbacks for this tutorial
+            Glut.glutMouseFunc(OnMouse);
+            Glut.glutMotionFunc(OnMove);
+
+            Gl.Enable(EnableCap.Blend);
+            Gl.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
+
+            // create our shader program
+            program = new ShaderProgram(VertexShader, FragmentShader);
+
+            // create our camera
+            camera = new Camera(new Vector3(0, 0, 50), Quaternion.Identity);
+            camera.SetDirection(new Vector3(0, 0, -1));
+
+            // set up the projection and view matrix
+            program.Use();
+            program["projection_matrix"].SetValue(Matrix4.CreatePerspectiveFieldOfView(0.45f, (float)width / height, 0.1f, 1000f));
+            program["model_matrix"].SetValue(Matrix4.Identity);
+
+            objectFile = new ObjLoader("enterprise/enterprise.obj", program);
+
+            // load the bitmap font for this tutorial
+            font = new BMFont("font24.fnt", "font24.png");
+            fontProgram = new ShaderProgram(BMFont.FontVertexSource, BMFont.FontFragmentSource);
+
+            fontProgram.Use();
+            fontProgram["ortho_matrix"].SetValue(Matrix4.CreateOrthographic(width, height, 0, 1000));
+            fontProgram["color"].SetValue(new Vector3(1, 1, 1));
+
+            information = font.CreateString(fontProgram, "OpenGL  C#  Tutorial  16");
+
+            watch = System.Diagnostics.Stopwatch.StartNew();
+
+            Glut.glutMainLoop();
+        }
+
+        private static void OnClose()
+        {
+            objectFile.Dispose();
+
+            program.DisposeChildren = true;
+            program.Dispose();
+            fontProgram.DisposeChildren = true;
+            fontProgram.Dispose();
+            font.FontTexture.Dispose();
+            information.Dispose();
+        }
+
+        private static void OnDisplay()
+        {
+
+        }
+
+        private static bool mouseDown = false;
+        private static int downX, downY;
+
+        private static void OnMouse(int button, int state, int x, int y)
+        {
+            // this method gets called whenever a new mouse button event happens
+            if (button == Glut.GLUT_RIGHT_BUTTON) mouseDown = (state == Glut.GLUT_DOWN);
+
+            // if the mouse has just been clicked then we hide the cursor and store the position
+            if (mouseDown)
+            {
+                Glut.glutSetCursor(Glut.GLUT_CURSOR_NONE);
+                downX = x;
+                downY = y;
+            }
+            else // unhide the cursor if the mouse has just been released
+                Glut.glutSetCursor(Glut.GLUT_CURSOR_LEFT_ARROW);
+        }
+
+        private static void OnMove(int x, int y)
+        {
+            // if the mouse move event is caused by glutWarpPointer then do nothing
+            if (x == downX && y == downY) return;
+
+            // move the camera when the mouse is down
+            if (mouseDown)
+            {
+                float yaw = (downX - x) * 0.002f;
+                camera.Yaw(yaw);
+
+                float pitch = (downY - y) * 0.002f;
+                camera.Pitch(pitch);
+
+                Glut.glutWarpPointer(downX, downY);
+            }
+        }
+
+        private static void OnRenderFrame()
+        {
+            watch.Stop();
+            float deltaTime = (float)watch.ElapsedTicks / System.Diagnostics.Stopwatch.Frequency;
+            watch.Restart();
+
+            // update our camera by moving it up to 5 units per second in each direction
+            if (down) camera.MoveRelative(Vector3.UnitZ * deltaTime * 5);
+            if (up) camera.MoveRelative(-Vector3.UnitZ * deltaTime * 5);
+            if (left) camera.MoveRelative(-Vector3.UnitX * deltaTime * 5);
+            if (right) camera.MoveRelative(Vector3.UnitX * deltaTime * 5);
+            if (space) camera.MoveRelative(Vector3.Up * deltaTime * 3);
+
+            // set up the viewport and clear the previous depth and color buffers
+            Gl.Viewport(0, 0, width, height);
+            Gl.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            // apply our camera view matrix to the shader view matrix (this can be used for all objects in the scene)
+            Gl.UseProgram(program);
+            program["view_matrix"].SetValue(camera.ViewMatrix);
+
+            // now draw the object file
+            if (wireframe) Gl.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Line);
+            objectFile.Draw();
+            Gl.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Fill);
+
+            // bind the font program as well as the font texture
+            Gl.UseProgram(fontProgram.ProgramID);
+            Gl.BindTexture(font.FontTexture);
+
+            // draw the tutorial information, which is static
+            information.Draw();
+
+            Glut.glutSwapBuffers();
+        }
+
+        private static void OnReshape(int width, int height)
+        {
+            Program.width = width;
+            Program.height = height;
+
+            Gl.UseProgram(program.ProgramID);
+            program["projection_matrix"].SetValue(Matrix4.CreatePerspectiveFieldOfView(0.45f, (float)width / height, 0.1f, 1000f));
+
+            Gl.UseProgram(fontProgram.ProgramID);
+            fontProgram["ortho_matrix"].SetValue(Matrix4.CreateOrthographic(width, height, 0, 1000));
+
+            information.Position = new Vector2(-width / 2 + 10, height / 2 - font.Height - 10);
+        }
+
+        private static void OnKeyboardDown(byte key, int x, int y)
+        {
+            if (key == 'w') up = true;
+            else if (key == 's') down = true;
+            else if (key == 'd') right = true;
+            else if (key == 'a') left = true;
+            else if (key == ' ') space = true;
+            else if (key == 27) Glut.glutLeaveMainLoop();
+        }
+
+        private static void OnKeyboardUp(byte key, int x, int y)
+        {
+            if (key == 'w') up = false;
+            else if (key == 's') down = false;
+            else if (key == 'd') right = false;
+            else if (key == 'a') left = false;
+            else if (key == ' ') space = false;
+            else if (key == 'q') wireframe = !wireframe;
+            else if (key == 'f')
+            {
+                fullscreen = !fullscreen;
+                if (fullscreen) Glut.glutFullScreen();
+                else
+                {
+                    Glut.glutPositionWindow(0, 0);
+                    Glut.glutReshapeWindow(1280, 720);
+                }
+            }
+        }
+
+        private static string VertexShader = @"
+#version 130
+
+in vec3 vertexPosition;
+in vec3 vertexNormal;
+in vec2 vertexUV;
+
+out vec3 normal;
+out vec2 uv;
+
+uniform mat4 projection_matrix;
+uniform mat4 view_matrix;
+uniform mat4 model_matrix;
+
+void main(void)
+{
+    normal = normalize((model_matrix * vec4(vertexNormal, 0)).xyz);
+    uv = vertexUV;
+
+    gl_Position = projection_matrix * view_matrix * model_matrix * vec4(vertexPosition, 1);
+}
+";
+
+        private static string FragmentShader = @"
+#version 130
+
+in vec3 normal;
+in vec2 uv;
+
+out vec4 fragment;
+
+uniform vec3 diffuse;
+uniform sampler2D texture;
+uniform float transparency;
+uniform bool useTexture;
+
+void main(void)
+{
+    vec3 light_direction = normalize(vec3(1, 1, 0));
+    float light = max(0.5, dot(normal, light_direction));
+    vec4 sample = (useTexture ? texture2D(texture, uv) : vec4(1, 1, 1, 1));
+    fragment = vec4(light * diffuse * sample.xyz, transparency * sample.a);
+}
+";
+    }
+}
